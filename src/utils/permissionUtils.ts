f8730d18c6bf68be53ca2f161ada7151e314f434
antra <ujjwalt616@gmail.com>
2026-01-22 15:41:47 +0530

    fixed caisno admins

--- src/utils/permissionUtils.ts
+++ src/utils/permissionUtils.ts
@@ -1,4 +1,5 @@
 import { Message, GuildMember } from "discord.js";
+import prisma from "./prisma";
 
 export const BOT_OWNER_ID = "1288340046449086567";
 
@@ -9,6 +10,20 @@ export async function canExecuteAdminCommand(message: Message, member?: GuildMem
     if (message.author.id === BOT_OWNER_ID) return true;
     if (message.author.id === message.guild?.ownerId) return true;
     if (member.permissions.has("Administrator")) return true;
+
+    // Check for Casino Admin
+    if (message.guild) {
+        const user = await prisma.user.findUnique({
+            where: {
+                discordId_guildId: {
+                    discordId: member.id,
+                    guildId: message.guild.id
+                }
+            },
+            select: { isCasinoAdmin: true }
+        });
+        if (user?.isCasinoAdmin) return true;
+    }
 
     return false;
 }
